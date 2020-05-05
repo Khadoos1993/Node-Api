65e0084ca1e0561fcf0fcd9bff75700be0e15d3d
khadoos1993 <kapil11102230@gmial.com>
2020-05-05 17:04:11 +0530

    bookid url not working

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -17,7 +17,7 @@ function booksController(Book) {
       const returnBooks = books.map((book) => {
         const newBook = book.toJSON();
         newBook.links = {};
-        newBook.links.self = `http://${req.headers.host}/api/books${book._id}`;
+        newBook.links.self = `http://${req.headers.host}/api/books/${book._id}`;
         return newBook;
       });
       return res.json(returnBooks);
